16da5b1586edcf47c2e97fce2d150af5c57bd34a
Álvaro <amonclus89@gmail.com>
2024-07-26 12:36:15 +0200

    a

--- membership_points/models/res_partner.py
+++ membership_points/models/res_partner.py
@@ -11,3 +11,12 @@ class ResPartner(models.Model):
         readonly=True,
         help="This shows the points that the customer has accumulated"
     )
+
+
+class PosSessionPartner(models.Model):
+    _inherit = 'pos.session'
+
+    def _loader_params_res_partner(self):
+        res = super()._loader_params_res_partner()
+        res.get("search_params").get("fields").append('membership_points')
+        return res
--- membership_points/static/src/js/payment_points.js
+++ membership_points/static/src/js/payment_points.js
@@ -7,12 +7,17 @@ import {useBus} from "@web/core/utils/hooks";
 const MembershipPaymentPoints = (PaymentScreen) => class extends PaymentScreen {
     setup() {
         super.setup()
+        let points = 0
         useBus(this.env.posbus, 'set-initial-points', this.setPoints);
     }
 
     async validateOrder(isForceValidate){
         super.validateOrder()
-        if(this.currentOrder.get_partner().is_membership_active) this.setPoints()
+        const price = this.currentOrder.get_total_with_tax() + this.currentOrder.get_rounding_applied()
+        if(this.currentOrder.get_partner().is_membership_active && price >= 5) {
+            this.points = Math.round(price*0.2)
+            this.setPoints()
+        }
     }
 
     async setPoints() {
@@ -20,15 +25,30 @@ const MembershipPaymentPoints = (PaymentScreen) => class extends PaymentScreen {
             const result = await this.rpc({
                 route: '/set_membership_points',                //http route to python script that makes the db request
                 params: {'partner_id': this.currentOrder.get_partner().id,
-                    'points': 10,
+                    'points': this.points,
                 }
             });
             if (result.error) {
-                this.state.error = result.error
+                console.log("Error")
             }
         } catch (error) {
-            this.state.error = 'Failed to update points'
+            console.log('Failed to update points')
         }
+        this.currentOrder.get_partner().membership_points += this.points
+
+    }
+
+    applyPoints() {
+        this.points = -this.currentOrder.get_partner().membership_points
+        const orderLines = this.currentOrder.get_orderlines()
+        let discount = Math.min(this.currentOrder.get_partner().membership_points * 0.5, 45)
+        if(discount > 45) discount = 45
+        for(let i = 0; i < orderLines.length; i++) {
+            orderLines[i].set_discount(discount)
+        }
+
+        this.setPoints()
+
     }
 
 };
